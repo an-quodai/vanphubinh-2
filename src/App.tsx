8d07daf19532794a8456cb574daef346d733ff96
an-quodai <an.tran511@hotmail.com>
2023-06-16 20:32:58 +0700

    Add core packages

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,46 @@
-import { GitHubBanner, Refine, WelcomePage } from "@refinedev/core";
+import { Refine, WelcomePage } from "@refinedev/core";
 import { RefineKbar, RefineKbarProvider } from "@refinedev/kbar";
 
 import routerBindings, {
   DocumentTitleHandler,
   UnsavedChangesNotifier,
 } from "@refinedev/react-router-v6";
-import dataProvider from "@refinedev/simple-rest";
+import dataProvider from "@refinedev/altogic";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import "./App.css";
+import { MantineProvider } from "@mantine/core";
+import { DatesProvider } from "@mantine/dates";
+import { Notifications } from "@mantine/notifications";
 
 function App() {
   return (
     <BrowserRouter>
-      <GitHubBanner />
       <RefineKbarProvider>
-        <Refine
-          dataProvider={dataProvider("https://api.fake-rest.refine.dev")}
-          routerProvider={routerBindings}
-          options={{
-            syncWithLocation: true,
-            warnWhenUnsavedChanges: true,
+        <MantineProvider
+          withGlobalStyles
+          withNormalizeCSS
+          theme={{
+            primaryShade: 7,
           }}
         >
-          <Routes>
-            <Route index element={<WelcomePage />} />
-          </Routes>
-          <RefineKbar />
-          <UnsavedChangesNotifier />
-          <DocumentTitleHandler />
-        </Refine>
+          <Notifications position="top-right" zIndex={2077} />
+          <DatesProvider settings={{ locale: "vi" }}>
+            <Refine
+              dataProvider={dataProvider("https://api.fake-rest.refine.dev")}
+              routerProvider={routerBindings}
+              options={{
+                syncWithLocation: true,
+                warnWhenUnsavedChanges: true,
+              }}
+            >
+              <Routes>
+                <Route index element={<WelcomePage />} />
+              </Routes>
+              <RefineKbar />
+              <UnsavedChangesNotifier />
+              <DocumentTitleHandler />
+            </Refine>
+          </DatesProvider>
+        </MantineProvider>
       </RefineKbarProvider>
     </BrowserRouter>
   );
