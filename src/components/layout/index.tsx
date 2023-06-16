798b75345b4cc4b1be66e6216a1ecbc91652d490
an-quodai <an.tran511@hotmail.com>
2023-06-16 20:47:43 +0700

    Add layout header and path for tsconfig

--- src/components/layout/index.tsx
+++ src/components/layout/index.tsx
@@ -45,26 +45,25 @@ export const Layout: React.FC<PropsWithChildren<{}>> = ({ children }) => {
           </Navbar>
         }
         header={
-          <></>
-          // <Header height={{ base: 50, md: 50 }} p="md">
-          //   <div
-          //     style={{
-          //       display: "flex",
-          //       alignItems: "center",
-          //       height: "100%",
-          //     }}
-          //   >
-          //     <MediaQuery largerThan="sm" styles={{ display: "none" }}>
-          //       <Burger
-          //         opened={opened}
-          //         onClick={() => setOpened((o) => !o)}
-          //         size="sm"
-          //         color={theme.colors.gray[6]}
-          //         mr="xl"
-          //       />
-          //     </MediaQuery>
-          //   </div>
-          // </Header>
+          <Header height={{ base: 50, md: 50 }} p="md">
+            <div
+              style={{
+                display: "flex",
+                alignItems: "center",
+                height: "100%",
+              }}
+            >
+              <MediaQuery largerThan="sm" styles={{ display: "none" }}>
+                <Burger
+                  opened={opened}
+                  onClick={() => setOpened((o) => !o)}
+                  size="sm"
+                  color={theme.colors.gray[6]}
+                  mr="xl"
+                />
+              </MediaQuery>
+            </div>
+          </Header>
         }
       >
         {/* <Breadcrumb /> */}
